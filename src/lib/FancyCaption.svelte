15bf47ec7e3c9b3daea8d8a75b1e829fb67a7782
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-06-25 01:09:39 +0200

    feat: :sparkles: zzzzzzzzzz

--- src/lib/FancyCaption.svelte
+++ src/lib/FancyCaption.svelte
@@ -5,8 +5,11 @@
 <div class="flex h-screen items-center justify-center bg-slate-400">
 	{#each text as char, i}
 		<div class="relative">
-			<p class="text-stroke-4 relative top-32 text-stroke-white font-aoeu text-9xl scale-105 text-center text-transparent">{char}</p>
-			<p class="text-stroke-4 text-stroke-black font-aoeu text-9xl text-indigo-500">
+			<p
+				class="text-stroke-white text-stroke-4 font-aoeu relative top-32 scale-105 text-center text-9xl text-transparent">
+				{char}
+			</p>
+			<p class="text-stroke-5 text-stroke-black font-aoeu z-0 text-9xl text-indigo-500">
 				{char}
 			</p>
 		</div>
