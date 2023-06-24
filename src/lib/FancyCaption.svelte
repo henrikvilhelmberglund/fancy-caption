2bcb2d7a3d7538e08ce7790943a8d6259cad3476
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-06-25 01:00:58 +0200

    feat: :sparkles: make more fancy

--- src/lib/FancyCaption.svelte
+++ src/lib/FancyCaption.svelte
@@ -1,10 +1,16 @@
 <script>
+	export let text;
 </script>
 
-<div class="flex h-screen items-center justify-center">
-	<p class="text-stroke-4 text-stroke-black font-aoeu absolute text-9xl text-indigo-500">
-		<slot />
-	</p>
+<div class="flex h-screen items-center justify-center bg-slate-400">
+	{#each text as char, i}
+		<div class="relative">
+			<p class="text-stroke-4 relative top-32 text-stroke-white font-aoeu text-9xl scale-105 text-center text-transparent">{char}</p>
+			<p class="text-stroke-4 text-stroke-black font-aoeu text-9xl text-indigo-500">
+				{char}
+			</p>
+		</div>
+	{/each}
 </div>
 
 <style>
