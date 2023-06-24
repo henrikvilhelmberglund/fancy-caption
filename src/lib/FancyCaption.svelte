e7f2958c6a342e638b792a0544f75ffbc036bf1c
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-06-25 00:47:28 +0200

    feat: :sparkles: add FancyCaption component

---
+++ src/lib/FancyCaption.svelte
@@ -0,0 +1,11 @@
+<script>
+</script>
+
+<div class="flex h-screen items-center justify-center">
+	<p class="text-stroke-4 text-stroke-black font-aoeu absolute text-9xl text-indigo-500">
+		<slot />
+	</p>
+</div>
+
+<style>
+</style>
