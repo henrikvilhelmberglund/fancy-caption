0dc6e7e03379c5c4ffd5aed6fb5dd30689893f52
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-06-25 15:27:50 +0200

    feat: :sparkles: add dynamic route

---
+++ src/routes/[input]/+page.server.js
@@ -0,0 +1,32 @@
+import fs from "fs";
+import puppeteer from "puppeteer";
+
+/** @type {import('./$types').Actions} */
+export const actions = {
+	default: async ({ params }) => {
+		const browser = await puppeteer.launch();
+
+		// Open a new page
+		const page = await browser.newPage();
+
+		// Set the viewport size to match your app's dimensions
+		await page.setViewport({ width: 1080, height: 160 });
+
+		// Navigate to your SvelteKit app's local development server URL
+		await page.goto(`http://localhost:5173/${params.input}`);
+
+		// Wait for your app to finish rendering (you might need to adjust the timeout based on your app's complexity)
+		await page.waitForTimeout(50);
+
+		// Capture a screenshot of the entire page
+		const screenshot = await page.screenshot({ fullPage: false, omitBackground: true });
+
+		// Save the screenshot as a PNG file
+		fs.writeFileSync(`${params.input}.png`, screenshot);
+
+		// Close the browser
+		await browser.close();
+
+		console.log(`Screenshot saved as ${params.input}.png`);
+	},
+};
---
+++ src/routes/[input]/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import FancyCaption from "$lib/FancyCaption.svelte";
+	import { page } from "$app/stores";
+
+	const text = $page.params.input;
+</script>
+
+<main>
+	<FancyCaption {text} />
+</main>
+<form method="POST">
+	<button>Render</button>
+</form>
+
+<!-- <Footer /> -->
+
+<style>
+</style>
