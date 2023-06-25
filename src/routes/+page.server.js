f5bd47cb7139880061386370222e3f4204c70c41
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-06-25 15:43:02 +0200

    feat: :sparkles: look through lines.txt and save a screenshot for each line

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -6,27 +6,31 @@ export const actions = {
 	default: async (event) => {
 		const browser = await puppeteer.launch();
 
-		// Open a new page
-		const page = await browser.newPage();
+		try {
+			const lines = fs
+				.readFileSync("src/lib/lines.txt", "utf-8")
+				.split("\n")
+				.map((item) => item.trim());
+			try {
+				const page = await browser.newPage();
+				await page.setViewport({ width: 1080, height: 160 });
 
-		// Set the viewport size to match your app's dimensions
-		await page.setViewport({ width: 1080, height: 160 });
-
-		// Navigate to your SvelteKit app's local development server URL
-		await page.goto("http://localhost:5173");
-
-		// Wait for your app to finish rendering (you might need to adjust the timeout based on your app's complexity)
-		await page.waitForTimeout(50);
-
-		// Capture a screenshot of the entire page
-		const screenshot = await page.screenshot({ fullPage: false, omitBackground: true });
-
-		// Save the screenshot as a PNG file
-		fs.writeFileSync("export.png", screenshot);
-
-		// Close the browser
-		await browser.close();
-
-		console.log("Screenshot saved as export.png");
+				let i = 0;
+				for (const line of lines) {
+					if (line.trim() !== "") {
+						await page.goto(`http://localhost:5173/${line}`);
+						await page.waitForTimeout(50);
+						const screenshot = await page.screenshot({ fullPage: false, omitBackground: true });
+						fs.writeFileSync(`./output/${i.toString().padStart(3, "0")}_${line}.png`, screenshot);
+						console.log(`Screenshot saved as ${i.toString().padStart(3, "0")}_${line}.png`);
+						i++;
+					}
+				}
+			} finally {
+				await browser.close();
+			}
+		} catch (error) {
+			console.error(error);
+		}
 	},
 };
