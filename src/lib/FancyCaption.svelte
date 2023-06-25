8cd892ba7c4803c37cec7d728f8941a7731675a7
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-06-25 15:27:35 +0200

    fix: :lipstick: show text at the top of the screen instead

--- src/lib/FancyCaption.svelte
+++ src/lib/FancyCaption.svelte
@@ -2,14 +2,14 @@
 	export let text;
 </script>
 
-<div class="z-[-100] flex h-screen items-center justify-center bg-transparent">
-	{#each text as char, i}
-		<div class="relative">
+<div class="flex h-full items-start justify-center bg-transparent">
+	{#each text as char}
+		<div class="-top-30 relative">
 			<p
 				class="font-aoeu text-stroke-white text-stroke-2 relative top-32 scale-105 text-center text-9xl text-transparent">
 				{char}
 			</p>
-			<p class=" font-aoeu text-shadow-lg my-shadow z-0 text-9xl text-indigo-500">
+			<p class="font-aoeu text-shadow-lg my-shadow z-0 text-9xl text-indigo-500">
 				{char}
 			</p>
 		</div>
